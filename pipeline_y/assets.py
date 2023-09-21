afad8611c17ba0bdce709714adf1e79d061991ef
Ibrahim Halil Koyuncu <koyuncuibrahim1453@gmail.com>
2023-09-21 19:48:43 +0300

    upload pipelines

---
+++ pipeline_y/assets.py
@@ -0,0 +1,14 @@
+import json
+import os
+
+import requests
+from dagster import asset # import the `dagster` library
+
+@asset # add the asset decorator to tell Dagster this is an asset
+def topstory_ids() -> None:
+    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
+
+    os.makedirs("data", exist_ok=True)
+    with open("data/topstory_ids.json", "w") as f:
+        json.dump(top_new_story_ids, f)
